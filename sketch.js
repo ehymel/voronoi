0d19c2a3ff83b96cfe2d446b5a7790ae4b8f3ec7
Ern at home <ehymel@practicetally.com>
2024-06-16 17:08:18 -0500

    Implement Delaunay triangulation

--- sketch.js
+++ sketch.js
@@ -3,15 +3,16 @@ let delaunay;
 
 function setup() {
     createCanvas(400, 400);
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 25; i++) {
         seedPoints[i] = createVector(random(width), random(height));
     }
-
-    delaunay = calculateDelaunay(seedPoints);
 }
 
 function draw() {
     background(255);
+
+    delaunay = calculateDelaunay(seedPoints);
+    let voronoi = delaunay.voronoi([0, 0, width, height]);
 
     for (let v of seedPoints) {
         stroke(0);
@@ -19,14 +20,28 @@ function draw() {
         point(v.x, v.y);
     }
 
-    noFill();
-    strokeWeight(1);
-    let {points, triangles} = delaunay;
-    for (let i = 0; i < triangles.length; i += 3) {
-        let a = 2 * triangles[i];
-        let b = 2 * triangles[i + 1];
-        let c = 2 * triangles[i + 2];
-        triangle(points[a], points[a + 1], points[b], points[b + 1], points[c], points[c + 1], );
+    // noFill();
+    // strokeWeight(1);
+    // let {points, triangles} = delaunay;
+    // for (let i = 0; i < triangles.length; i += 3) {
+    //     let a = 2 * triangles[i];
+    //     let b = 2 * triangles[i + 1];
+    //     let c = 2 * triangles[i + 2];
+    //     triangle(points[a], points[a + 1], points[b], points[b + 1], points[c], points[c + 1], );
+    // }
+
+    let polygons = voronoi.cellPolygons();
+    let cells = Array.from(polygons);
+
+    for (let poly of cells) {
+        stroke(0);
+        strokeWeight(1);
+        noFill();
+        beginShape();
+        for (let i = 0; i < poly.length; i++) {
+            vertex(poly[i][0], poly[i][1]);
+        }
+        endShape();
     }
 }
 
