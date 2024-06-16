506d0c8ac926eda234eccc46bef3d17b3e9478c2
Ern at home <ehymel@practicetally.com>
2024-06-16 16:53:31 -0500

    Implement Delaunay triangulation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+let seedPoints = [];
+let delaunay;
+
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(400, 400);
+    for (let i = 0; i < 100; i++) {
+        seedPoints[i] = createVector(random(width), random(height));
+    }
+
+    delaunay = calculateDelaunay(seedPoints);
 }
 
 function draw() {
-    background(0);
+    background(255);
+
+    for (let v of seedPoints) {
+        stroke(0);
+        strokeWeight(4);
+        point(v.x, v.y);
+    }
+
+    noFill();
+    strokeWeight(1);
+    let {points, triangles} = delaunay;
+    for (let i = 0; i < triangles.length; i += 3) {
+        let a = 2 * triangles[i];
+        let b = 2 * triangles[i + 1];
+        let c = 2 * triangles[i + 2];
+        triangle(points[a], points[a + 1], points[b], points[b + 1], points[c], points[c + 1], );
+    }
+}
+
+function calculateDelaunay(points) {
+    let pointsArray = [];
+    for (let v of points) {
+        pointsArray.push(v.x, v.y);
+    }
+    return new d3.Delaunay(pointsArray);
 }
